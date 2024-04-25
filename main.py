5bffb204b4d16cd8a645c65ee353147cf93b5ed9
Pr1ZrAk77 <m07kochet@yandex.ru>
2024-04-25 17:12:04 +0300

    #development

--- main.py
+++ main.py
@@ -3,15 +3,14 @@ import sqlite3
 from telegram.ext import Application, MessageHandler, filters, Updater, ConversationHandler
 from telegram.ext import CommandHandler
 from telegram import ReplyKeyboardMarkup
-from datetime import *
 from config import BOT_TOKEN
 
 
-#logging.basicConfig(
-#    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.DEBUG
-#)
-#
-#logger = logging.getLogger(__name__)
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.DEBUG
+)
+
+logger = logging.getLogger(__name__)
 
 
 async def start(update, context):
@@ -26,7 +25,7 @@ async def start(update, context):
 
 async def help_command(update, context):
     """Отправляет сообщение когда получена команда /help"""
-    await update.message.reply_text("Я пока не умею помогать... Я только ваше эхо.")
+    await update.message.reply_text("Я пока не умею помогать...")
 
 
 async def teacher(update, context):
@@ -40,31 +39,221 @@ async def teacher(update, context):
 
 
 async def student(update, context):
-    usernow = update.effective_user.first_name
-    usersdb = []
-    connection = sqlite3.connect('data/group.db')
-    cursor = connection.cursor()
-    cursor.execute('SELECT user FROM Users')
-    user = cursor.fetchall()
-    for user in user:
-        usersdb.append(user[0])
-    if usernow in usersdb:
-        await update.message.reply_text("rhjcfdxbr")
-        return quiz
-    connection.close()
+    reply_keyboard = [['Пройти викторину', 'Посмотреть результаты']]
+    markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=False)
+    await update.message.reply_html('Выберите, что вы хотите сделать', reply_markup=markup)
+    return studentсhoice
 
 
-async def quiz(update, context):
-    q = []
+async def studentсhoice(update, context):
+    reply_keyboard = []
+    a = update.message.text
+    if a == 'Пройти викторину':
+        usernow = update.effective_user.username
+        usersdb = []
+        connection = sqlite3.connect('data/group.db')
+        cursor = connection.cursor()
+        cursor.execute('SELECT user, quizz FROM Users')
+        user = cursor.fetchall()
+        for user in user:
+            usersdb.append(user[0])
+            reply_keyboard.append([user[1]])
+        if usernow in usersdb:
+            markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=False)
+            await update.message.reply_text('Выбери квиз', reply_markup=markup)
+            return quwest1
+        connection.close()
+    elif a == 'Посмотреть результаты':
+        return ConversationHandler.END
+        return results
+
+
+async def quwest1(update, context):
+    global qname
+    global qu
+    qname = update.message.text
     connection = sqlite3.connect('data/quiz.db')
     cursor = connection.cursor()
-    cursor.execute(f'SELECT * FROM {namequiz}')
-    user = cursor.fetchall()
-    for user in user:
-        q.append(user)
-        await update.message.reply_text(user)
-    print(q)
+    cursor.execute(f'SELECT * FROM {qname}')
+    qu = cursor.fetchall()
+    st = qu[0]
+    reply_keyboard = [[str(st[2]), str(st[3]), str(st[4]), str(st[5])]]
+    markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=False)
+    await update.message.reply_text(str(st[1]), reply_markup=markup)
     connection.close()
+    return quwest2
+
+
+async def quwest2(update, context):
+    a = update.message.text
+    global r
+    r = 0
+    connection = sqlite3.connect('data/quiz.db')
+    cursor = connection.cursor()
+    cursor.execute(f'SELECT answer1 FROM {qname}')
+    ta = cursor.fetchall()
+    if a == str(*ta[0]):
+        r += 1
+    connection.close()
+    st = qu[1]
+    reply_keyboard = [[str(st[2]), str(st[3]), str(st[4]), str(st[5])]]
+    markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=False)
+    await update.message.reply_text(str(st[1]), reply_markup=markup)
+    return quwest3
+
+
+async def quwest3(update, context):
+    a = update.message.text
+    connection = sqlite3.connect('data/quiz.db')
+    cursor = connection.cursor()
+    cursor.execute(f'SELECT answer1 FROM {qname}')
+    ta = cursor.fetchall()
+    if a == str(*ta[1]):
+        r = 1
+    connection.close()
+    st = qu[2]
+    reply_keyboard = [[str(st[2]), str(st[3]), str(st[4]), str(st[5])]]
+    markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=False)
+    await update.message.reply_text(str(st[1]), reply_markup=markup)
+    return quwest4
+
+
+async def quwest4(update, context):
+    a = update.message.text
+    connection = sqlite3.connect('data/quiz.db')
+    cursor = connection.cursor()
+    cursor.execute(f'SELECT answer1 FROM {qname}')
+    ta = cursor.fetchall()
+    if a == str(*ta[2]):
+        r = 1
+    connection.close()
+    st = qu[3]
+    reply_keyboard = [[str(st[2]), str(st[3]), str(st[4]), str(st[5])]]
+    markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=False)
+    await update.message.reply_text(str(st[1]), reply_markup=markup)
+    return quwest5
+
+
+async def quwest5(update, context):
+    a = update.message.text
+    connection = sqlite3.connect('data/quiz.db')
+    cursor = connection.cursor()
+    cursor.execute(f'SELECT answer1 FROM {qname}')
+    ta = cursor.fetchall()
+    if a == str(*ta[3]):
+        r = 1
+    connection.close()
+    st = qu[4]
+    reply_keyboard = [[str(st[2]), str(st[3]), str(st[4]), str(st[5])]]
+    markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=False)
+    await update.message.reply_text(str(st[1]), reply_markup=markup)
+    return quwest6
+
+
+async def quwest6(update, context):
+    a = update.message.text
+    connection = sqlite3.connect('data/quiz.db')
+    cursor = connection.cursor()
+    cursor.execute(f'SELECT answer1 FROM {qname}')
+    ta = cursor.fetchall()
+    if a == str(*ta[4]):
+        r = 1
+    connection.close()
+    st = qu[5]
+    reply_keyboard = [[str(st[2]), str(st[3]), str(st[4]), str(st[5])]]
+    markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=False)
+    await update.message.reply_text(str(st[1]), reply_markup=markup)
+    return quwest7
+
+
+async def quwest7(update, context):
+    a = update.message.text
+    connection = sqlite3.connect('data/quiz.db')
+    cursor = connection.cursor()
+    cursor.execute(f'SELECT answer1 FROM {qname}')
+    ta = cursor.fetchall()
+    if a == str(*ta[5]):
+        r = 1
+    connection.close()
+    st = qu[6]
+    reply_keyboard = [[str(st[2]), str(st[3]), str(st[4]), str(st[5])]]
+    markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=False)
+    await update.message.reply_text(str(st[1]), reply_markup=markup)
+    return quwest8
+
+
+async def quwest8(update, context):
+    a = update.message.text
+    connection = sqlite3.connect('data/quiz.db')
+    cursor = connection.cursor()
+    cursor.execute(f'SELECT answer1 FROM {qname}')
+    ta = cursor.fetchall()
+    if a == str(*ta[6]):
+        r = 1
+    connection.close()
+    st = qu[7]
+    reply_keyboard = [[str(st[2]), str(st[3]), str(st[4]), str(st[5])]]
+    markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=False)
+    await update.message.reply_text(str(st[1]), reply_markup=markup)
+    return quwest9
+
+
+async def quwest9(update, context):
+    a = update.message.text
+    connection = sqlite3.connect('data/quiz.db')
+    cursor = connection.cursor()
+    cursor.execute(f'SELECT answer1 FROM {qname}')
+    ta = cursor.fetchall()
+    if a == str(*ta[7]):
+        r = 1
+    connection.close()
+    st = qu[8]
+    reply_keyboard = [[str(st[2]), str(st[3]), str(st[4]), str(st[5])]]
+    markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=False)
+    await update.message.reply_text(str(st[1]), reply_markup=markup)
+    return quwest10
+
+
+async def quwest10(update, context):
+    a = update.message.text
+    connection = sqlite3.connect('data/quiz.db')
+    cursor = connection.cursor()
+    cursor.execute(f'SELECT answer1 FROM {qname}')
+    ta = cursor.fetchall()
+    if a == str(*ta[8]):
+        r = 1
+    connection.close()
+    st = qu[9]
+    reply_keyboard = [[str(st[2]), str(st[3]), str(st[4]), str(st[5])]]
+    markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=False)
+    await update.message.reply_text(str(st[1]), reply_markup=markup)
+    return resultq
+
+
+async def resultq(update, context):
+    a = update.message.text
+    username = update.effective_user.first_name
+    connection = sqlite3.connect('data/quiz.db')
+    cursor = connection.cursor()
+    cursor.execute(f'SELECT answer1 FROM {qname}')
+    ta = cursor.fetchall()
+    if a == str(*ta[9]):
+        r = 1
+    connection.close()
+    connection = sqlite3.connect('data/result.db')
+    cursor = connection.cursor()
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS Users (
+        id INTEGER PRIMARY KEY,
+        user TEXT NOT NULL,
+        quizname TEXT NOT NULL,
+        quizres TEXT NOT NULL
+        )
+        ''')
+    cursor.execute(f'''INSERT INTO Users (user, quizname, quizres) VALUES (?, ?, ?)''', (username, qname, r))
+    connection.close()
+    await update.message.reply_text(f'Поздравляю, вы прошли квиз!')
+    return ConversationHandler.END
 
 
 async def create_a_quiz(update, context):
@@ -170,6 +359,13 @@ async def create_a_group(update, context):
 
 
 async def Newgroup(update, context):
+    await update.message.reply_text(rf"Введите квиза в котором должны учавствовать пользователи")
+    return addq
+
+
+async def addq(update, context):
+    global qname
+    qname = update.message.text
     await update.message.reply_text(rf"Введите нового пользователя группы.")
     return adduser
 
@@ -182,10 +378,11 @@ async def adduser(update, context):
     cursor.execute('''
     CREATE TABLE IF NOT EXISTS Users (
     id INTEGER PRIMARY KEY,
-    user TEXT NOT NULL
+    user TEXT NOT NULL,
+    quizz TEXT NOT NULL
     )
     ''')
-    cursor.execute(f'''INSERT INTO Users (user) VALUES (?)''', (username, ))
+    cursor.execute(f'''INSERT INTO Users (user, quizz) VALUES (?, ?)''', (username, qname))
     connection.commit()
     connection.close()
     await update.message.reply_text(rf"Введите нового пользователя группы."
@@ -219,7 +416,27 @@ def main():
     application.add_handler(CommandHandler("start", start))
     application.add_handler(CommandHandler("help", help_command))
     application.add_handler(CommandHandler("teacher", teacher))
-    application.add_handler(CommandHandler("student", student))
+    conv_handler = ConversationHandler(
+        entry_points=[CommandHandler('student', student)],
+
+        states={
+            studentсhoice: [MessageHandler(filters.TEXT & ~filters.COMMAND, studentсhoice)],
+            quwest1: [MessageHandler(filters.TEXT & ~filters.COMMAND, quwest1)],
+            quwest2: [MessageHandler(filters.TEXT & ~filters.COMMAND, quwest2)],
+            quwest3: [MessageHandler(filters.TEXT & ~filters.COMMAND, quwest3)],
+            quwest4: [MessageHandler(filters.TEXT & ~filters.COMMAND, quwest4)],
+            quwest5: [MessageHandler(filters.TEXT & ~filters.COMMAND, quwest5)],
+            quwest6: [MessageHandler(filters.TEXT & ~filters.COMMAND, quwest6)],
+            quwest7: [MessageHandler(filters.TEXT & ~filters.COMMAND, quwest7)],
+            quwest8: [MessageHandler(filters.TEXT & ~filters.COMMAND, quwest8)],
+            quwest9: [MessageHandler(filters.TEXT & ~filters.COMMAND, quwest9)],
+            quwest10: [MessageHandler(filters.TEXT & ~filters.COMMAND, quwest10)],
+            resultq: [MessageHandler(filters.TEXT & ~filters.COMMAND, resultq)],
+        },
+
+        fallbacks=[CommandHandler("cancel", cancel)],
+    )
+    application.add_handler(conv_handler)
     application.add_handler(CommandHandler("createquiz", create_a_quiz))
 
     conv_handler = ConversationHandler(
@@ -255,6 +472,7 @@ def main():
         entry_points=[CommandHandler('Newgroup', Newgroup)],
 
         states={
+            addq: [MessageHandler(filters.TEXT & ~filters.COMMAND, addq)],
             adduser: [MessageHandler(filters.TEXT & ~filters.COMMAND, adduser)],
         },
 
